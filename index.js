03b98ba37da7814274be728ab5c4068849db6342
Shirisha-g-shiri <144538812+Shirisha-g-shiri@users.noreply.github.com>
2025-02-03 10:48:43 +0530

    db config

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 
 app.listen('5000', ()=> console.log('server running on 5000'))
 
-mongoose.connect('mongodb://localhost:27017/sample')
+mongoose.connect('mongodb+srv://2216115:123456shiri@myproject.rf1bs.mongodb.net/?retryWrites=true&w=majority&appName=myProject')
     .then(()=> console.log('database connected..'))
     .catch((err) => console.log(err))
 
